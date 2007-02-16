385c0d064d0baf38dd825bf26deb704933f918dc
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2007-02-16 15:42:58 +0000

    cleanup debug

--- test/setup.sh
+++ test/setup.sh
@@ -3,22 +3,18 @@
 eval `scramv1 ru -sh`
 
 COLLECTOR_NODE=$1
-echo $COLLECTOR_NODE
 
 HOSTNAME=$(echo `/bin/hostname` | sed 's/\//\\\//g')
-echo "The hostname is = $HOSTNAME"
 
 TEST_PATH=$(echo "${PWD}" | sed 's/\//\\\//g')
-echo "The current directory is = $PWD"
 
 MWC_LIB1="${LOCALRT}/lib/${SCRAM_ARCH}/libDQMSiPixelMonitorClient.so"
-echo "Looking for the MonitorWebClient library... $MWC_LIB1"
 if [ ! -f $MWC_LIB1 ]; then
-    echo "Not Found! Will pick it up from the release area..."
+    echo "Library not found! Will pick it up from the release area..."
     
 MWC_LIB1="${CMSSW_RELEASE_BASE}/lib/${SCRAM_ARCH}/libDQMSiPixelMonitorClient.so"
 else 
-    echo "Found!"
+    echo "Library found!"
 fi
 
 MWC_LIB=$(echo "$MWC_LIB1" | sed 's/\//\\\//g')
@@ -27,13 +23,13 @@ echo $MWC_LIB1
 SERVED_DIR="http://${HOSTNAME}:1972/temporary"
 
 if [ -e profile.xml ]; then
-    rm profile.xml
+    rm -f profile.xml
 fi 
 if [ -e monClient.xml ]; then
-    rm SiPixelWebClient.xml
+    rm -f SiPixelWebClient.xml
 fi
 if [ -e startMonitorClient ]; then
-    rm startMonitorClient
+    rm -f startMonitorClient
 fi
 
 sed -e "s/.portn/1972/g" -e "s/.host/${HOSTNAME}/g" -e "s/.pwd/${TEST_PATH}/g" -e "s/.libpath/${MWC_LIB}/g" .profile.xml > profile.xml

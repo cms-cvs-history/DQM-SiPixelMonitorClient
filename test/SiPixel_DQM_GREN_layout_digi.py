dad42263412a70422acd3e0526991ab024256908
Petra Merkel <sha1-79b10ece386a5dca3be2f40efb409853dbaaa2a2@cern.ch>
2008-07-04 11:25:38 +0000

    remove unneccessary files

--- test/SiPixel_DQM_GREN_layout_digi.py
+++
@@ -1,7 +0,0 @@
-def pixellayout(i, p, *rows): i["Layouts/SiPixel Digi Layouts/" + p] = DQMItem(layout=rows)
-
-pixellayout(dqmitems, "SiPixel_Digi_Summary",
-  ["Tracker/SUMDIG_adc_PixelBarrel",
-   "Tracker/SUMDIG_ndigis_PixelBarrel"],
-  ["Tracker/SUMDIG_adc_PixelEndcap",
-   "Tracker/SUMDIG_ndigis_PixelEndcap"])
